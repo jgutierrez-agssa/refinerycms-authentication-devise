e6d4863586954287c5ac6492b575f67b42d47b6e
jgutierrez-agssa <jgutierrez@agssa.net>
2025-05-19 20:33:49 +0000

    reparando zeitwerk

--- app/decorators/controllers/action_controller_base_decorator.rb
+++ app/decorators/controllers/action_controller_base_decorator.rb
@@ -1,25 +1,32 @@
-require "refinery/authentication/devise/authorisation_manager"
+# app/decorators/controllers/action_controller_base_decorator.rb
+require_dependency 'refinery/authentication/devise/role'
+require_dependency 'refinery/authentication/devise/authorisation_manager'
 
-module RefineryAuthenticationDeviseActionControllerBaseDecoration
-  def self.prepended(base)
-    base.prepend_before_action :detect_authentication_devise_user!
-  end
+module Controllers
+  module ActionControllerBaseDecorator
+    def self.prepended(base)
+      base.prepend_before_action :detect_authentication_devise_user!
+    end
 
-  protected
-  def refinery_users_exist?
-    Refinery::Authentication::Devise::Role[:refinery].users.any?
-  end
+    protected
 
-  private
-  def refinery_authorisation_manager
-    @refinery_authorisation_manager ||= ::Refinery::Authentication::Devise::AuthorisationManager.new
-  end
+    def refinery_users_exist?
+      # Accede al namespace completo desde el root
+      ::Refinery::Authentication::Devise::Role[:refinery].users.any?
+    end
 
-  def detect_authentication_devise_user!
-    if current_authentication_devise_user
-      refinery_authorisation_manager.set_user!(current_authentication_devise_user)
+    private
+
+    def refinery_authorisation_manager
+      @refinery_authorisation_manager ||= ::Refinery::Authentication::Devise::AuthorisationManager.new
+    end
+
+    def detect_authentication_devise_user!
+      if current_authentication_devise_user
+        refinery_authorisation_manager.set_user!(current_authentication_devise_user)
+      end
     end
   end
 end
 
-ActionController::Base.send :prepend, RefineryAuthenticationDeviseActionControllerBaseDecoration
+ActionController::Base.prepend Controllers::ActionControllerBaseDecorator
--- app/decorators/controllers/refinery/application_controller_decorator.rb
+++ app/decorators/controllers/refinery/application_controller_decorator.rb
@@ -1,7 +1,18 @@
-Refinery::ApplicationController.module_eval do
-  private
-  def authorisation_manager
-    # defined in app/decorators/controllers/action_controller_base_decorator.rb
-    refinery_authorisation_manager
+module Controllers
+  module Refinery
+    module ApplicationControllerDecorator
+      private
+
+      def authorisation_manager
+        # Método definido en action_controller_base_decorator.rb
+        refinery_authorisation_manager
+      end
+    end
   end
 end
+
+# Precargar la clase original
+require_dependency "refinery/application_controller"
+
+# Aplicar el decorador
+Refinery::ApplicationController.prepend Controllers::Refinery::ApplicationControllerDecorator
